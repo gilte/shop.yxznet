5e4e5ab0f8851b2ba6febea77e84ca8b047b61f7
Gilte <75944151+gilte@users.noreply.github.com>
2025-07-30 01:19:35 -0300

    separaçao da logica do backend pra um outro repositorio

--- src/app/cadastro/page.tsx
+++ src/app/cadastro/page.tsx
@@ -24,7 +24,7 @@ export default function CadastroPage() {
     setIsLoading(true);
 
     try {
-      const response = await fetch('http://localhost:3000/api/cadastro', {
+      const response = await fetch('http://localhost:5000/api/auth/register', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -23,7 +23,7 @@ export default function LoginPage() {
     setIsLoading(true);
 
     try {
-      const response = await fetch('http://localhost:3000/api/login', {
+      const response = await fetch('http://localhost:5000/api/auth/login', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
